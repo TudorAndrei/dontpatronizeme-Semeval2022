681ab623e96c232f7a5cd2259bc843be5b0de5ce
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2022-01-18 00:25:37 +0200

    Added CONTRIB file, and new transformers
    New transformer approach:
    Train on general classes then finetune for subclasses

--- task2/trainer.py
+++ task2/trainer.py
@@ -1,29 +1,50 @@
+import logging as log
 import os
+import warnings
 
 from data_utils import DPMDataModule
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
-from transformer import BaseBert, DistillBert, RoBERTa
+from transformer import Bert, DistillBert, RoBERTa
+from transformers import logging
 from whos_there.callback import NotificationCallback
 from whos_there.senders.discord import DiscordSender
 
+logging.set_verbosity_warning()
+log.getLogger("pytorch_lightning").setLevel(log.WARNING)
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-BATCH_SIZE = 8
+seed_everything(42)
+
+BATCH_SIZE = 1
 NW = 8
-EPOCHS = 199
+EPOCHS = 100
 
 web_hook = "https://discord.com/api/webhooks/929728317408571452/rDR6JgNQkEKhAGWDckVtUQWB-DZh2Nqpv3rfWrU1ziFoeX37iPAG-CLU0O_n1wlakPp-"
 
 
 model_config = {
+    "bert": {
+        "hf_name": "bert-base-uncased",
+        "model": Bert,
+    },
+    "bert-multi-lingual": {
+        "hf_name": "bert-base-multilingual-uncased",
+        "model": Bert,
+    },
+
     "hatexplain": {
         "hf_name": "Hate-speech-CNERG/bert-base-uncased-hatexplain",
-        "model": BaseBert,
+        "model": Bert,
     },
     "distillbert": {
         "hf_name": "bhadresh-savani/distilbert-base-uncased-emotion",
+        "model": DistillBert,
+    },
+    "distillbert-multi": {
+        "hf_name": "distilbert-base-multilingual-cased",
         "model": DistillBert,
     },
     "distillroberta": {
@@ -33,12 +54,22 @@ model_config = {
 }
 
 if __name__ == "__main__":
-    # model_name = "hatexplain"
-    model_name="distillbert"
+    model_name = "bert"
+    # model_name = "bert-multi-lingual"
+    model_name = "hatexplain"
+
+    # model_name = "distillbert"
+    # model_name = "distillbert-multi"
     # model_name = "distillroberta"
+    num_outputs = 3
     model = model_config[model_name]
-    data = DPMDataModule(batch_size=BATCH_SIZE, num_workers=NW, model=model["hf_name"])
-    model = model["model"](model=model["hf_name"])
+    data = DPMDataModule(
+        batch_size=BATCH_SIZE,
+        num_workers=NW,
+        model=model["hf_name"],
+        num_outputs=num_outputs,
+    )
+    model = model["model"](model=model["hf_name"], n_classes=num_outputs)
     logger = TensorBoardLogger("tb_logs", name=f"{model_name}")
 
     trainer = Trainer(
@@ -56,13 +87,14 @@ if __name__ == "__main__":
                     )
                 ]
             ),
-            ModelCheckpoint(
-                monitor="val/val_loss",
-                mode="min",
-                dirpath=f"models/{model_name}",
-                filename="radar-epoch{epoch:02d}-val_loss{val/val_loss:.2f}",
-                auto_insert_metric_name=False,
-            ),
+            # LearningRateMonitor(logging_interval="step"),
+            # ModelCheckpoint(
+            #     monitor="val/val_loss",
+            #     mode="min",
+            #     dirpath=f"models/{model_name}",
+            #     filename="radar-epoch{epoch:02d}-val_loss{val/val_loss:.2f}",
+            #     auto_insert_metric_name=False,
+            # ),
             EarlyStopping(monitor="val/val_loss", patience=10),
         ],
     )
